54884a0e4861c5e947f8bb4cef43c0f636d707cb
Hesli Azevedo <sis-hesliazevedo@uniguacu.edu.br>
2019-07-01 19:46:04 -0300

    Umas mudancas

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,10 +2,11 @@
 int laco = 9;
 int calc = 10;
 int text = 11;
-int video = 12;
+int fechar = 12;
 
 //Global Scope
 int iniciado = 0;
+int laco_iniciado = 0;
 
 void setup() {
   //Botoes
@@ -20,6 +21,9 @@ void setup() {
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+
+  //Extras
+  
 }
 
 void loop() {
@@ -30,18 +34,34 @@ void loop() {
     }
 
   //Evento para iniciar o circuito
-  if(digitalRead(8) == HIGH){
+  if(digitalRead(8) == HIGH && iniciado == 0){
         iniciado = 1; 
-        loop();
         digitalWrite(1, HIGH);   
+        Serial.begin(9600);
+        Serial.println("Ligado");
     }
+    
+    if(digitalRead(8) == HIGH && iniciado == 1){
+           iniciado = 0; 
+           digitalWrite(1, LOW);   
+           Serial.begin(9600);
+           Serial.println("Desligado");
+    }
+    
+  
   if(digitalRead(laco) == HIGH){
+        laco_iniciado = 1;
+    }
+
+  if(laco_iniciado == 1){
         int c = 0; 
         while(c > 0){
              digitalWrite(1, LOW);
              delay(1000);
              digitalWrite(1, HIGH);
              c = c+1;
+             Serial.begin(9600);
+             Serial.println(c);
           }
     }
 }
